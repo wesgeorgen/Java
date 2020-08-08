a64eba253c4b6709e6728d70d494d67e388b46e1
wesgeorgen <camargo.wesley2012@gmail.com>
2020-08-07 22:17:14 -0300

    treinando git

---
+++ Treinando.java
@@ -0,0 +1,5 @@
+public class Treinando {   // To save as "Treinando.java" under "~/myProject"
+   public static void main(String[] args) {
+      System.out.println("voce esta Treinando?");
+   }
+}
